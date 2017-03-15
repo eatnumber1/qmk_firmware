a5041fe8a15e723de01ed4306fcd7deaa4414b92
Russell Harmon <eatnumber1@gmail.com>
2017-03-14 21:42:36 -0700

    Fix horizontal alignment

--- keyboards/tv44/keymaps/eatnumber1/keymap.c
+++ keyboards/tv44/keymaps/eatnumber1/keymap.c
@@ -10,10 +10,10 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_L0] = KEYMAP_TV44(
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC,
-    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_ENT,
-    KC_LCTL, KC_LGUI, KC_LALT,    LT(1, KC_SPC), LT(2, KC_SPC),            MO(3),   KC_RSFT,         KC_RCTL
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC,
+    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_ENT,
+    KC_LCTL, KC_LGUI, KC_LALT,       LT(1, KC_SPC), LT(2, KC_SPC),          MO(3),   KC_RSFT,         KC_RCTL
   ),
 
   [_L1] = KEYMAP_TV44(
